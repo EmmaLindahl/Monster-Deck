c448d142a6b6055872288c845049d4860bb7932b
Emma Lindahl <emma.sofia.dahl@gmail.com>
2024-10-23 20:56:55 +0200

    Setting up

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This will be an awesome site!
         </p>
         <a
           className="App-link"
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Monsters Bite! 
         </a>
+        <div>Raaaaaaaawr!</div>
       </header>
     </div>
   );
