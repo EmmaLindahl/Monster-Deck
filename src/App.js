0c69d76575fe42d83f786cefcc4862a8445e9ede
Emma Lindahl <emma.sofia.dahl@gmail.com>
2024-10-24 11:26:16 +0200

    Added Fetch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import react, {useEffect, useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState(null)
+
+  useEffect(() =>{
+    fetch('https://monsters-of-grimthornforest-backend.onrender.com/')
+    .then(response => response.json())
+    .then(data => {setData(data)})
+    .catch(error => {console.error("Error fetching data:", error)})
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This will be an awesome site!
+          {data ? `A wherewolf is howling in the distant "${data.WhereWolf}"` : 'Loading...'}
         </p>
         <a
           className="App-link"
